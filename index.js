4db600aaf85f451e7c6f7203c43f81521f0176be
Sahil <Sahilsid2001@gmail.com>
2024-02-21 13:05:50 +0530

    Create POST API Successsfully

--- index.js
+++ index.js
@@ -2,16 +2,14 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser")
-const Post = require("./postSchema.js")
-
+const Posts = require("./postSchema.js")
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
 
-
-
 // ***********MONGODB CONNECTION**********
+
 
 mongoose.connect("mongodb://127.0.0.1:27017/Trial")
 .then(()=>{
@@ -22,28 +20,32 @@ mongoose.connect("mongodb://127.0.0.1:27017/Trial")
 })
 
 
+
 // ***********API***************
 
 
 app.get("/",(req,res)=>{
-    return res.render('index.html')
+    return res.redirect('index.html')
 })
 
 app.post("/post",async(req,res)=>{
-    const name = req.body
-    const message = req.body
-
+    const {name, message} = req.body
     
     try {
-        const  data = new Post({name,message})
+        const  data = new Posts({name,message})
         const savedPost = await data.save()
     
-        if(savedPost){
-            return res.send("Success");
-        }
-        else{
-            return res.send("Some Error Occured");
-        }
+        return res.redirect("index.html")
+    } catch (error) {
+            res.send("Error hai kuch")
+    }
+})
+
+app.get("/allPost",async(req,res)=>{
+    try {
+        const  data =await Posts.find({});
+    
+        return res.send(data)
     } catch (error) {
             res.send("Error hai kuch")
     }
